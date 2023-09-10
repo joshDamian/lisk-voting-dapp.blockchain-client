d5fceca11f3280905eeebf1f9a3462ad25ae73e7
Avoaja Joshua Akachi <avoajajoshua@gmail.com>
2023-09-10 16:32:38 +0100

    fix: underscore unused app variable

--- src/app/plugins.ts
+++ src/app/plugins.ts
@@ -1,4 +1,4 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { Application } from 'lisk-sdk';
 
-export const registerPlugins = (app: Application): void => {};
+export const registerPlugins = (_: Application): void => {};

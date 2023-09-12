7576b6b9f1b872393d45009ef20cb604409cd62a
Avoaja Joshua Akachi <avoajajoshua@gmail.com>
2023-09-12 04:44:29 +0100

    feat: add dashboard plugin for client

--- src/app/plugins.ts
+++ src/app/plugins.ts
@@ -1,4 +1,7 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { Application } from 'lisk-sdk';
+import { DashboardPlugin } from '@liskhq/lisk-framework-dashboard-plugin';
 
-export const registerPlugins = (_: Application): void => {};
+export const registerPlugins = (app: Application): void => {
+	app.registerPlugin(new DashboardPlugin());
+};
